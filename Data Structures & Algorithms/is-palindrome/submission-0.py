f6ad38ad5a8f2fb39cac3e3b683664468f70f60e
Vatsalya Rastogi <vatsalya.rastogi1620004@gmail.com>
2026-07-31 13:36:37 -0400

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        n = len(s)
+        l = 0
+        r = n - 1
+        s=s.lower()
+        while l < r:
+            if not s[l].isalnum():
+                l+=1
+                continue
+            if not s[r].isalnum():
+                r-=1
+                continue
+            if s[l] != s[r]:
+                print(s[l], s[r])
+                return False
+            l+=1
+            r-=1
+        return True
